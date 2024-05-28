0250eb35402aaf0a229b1ba9750a61f147e3b4b1
anonymous6937 <wensongsoung@gmail.com>
2024-05-28 16:52:38 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -19,3 +19,22 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+	const lazyImages = document.querySelectorAll('.lazy');
+
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			const img = entry.target;
+			img.src = img.dataset.src;
+			img.classList.remove('lazy');
+			observer.unobserve(img);
+		}
+		});
+	});
+
+	lazyImages.forEach(img => {
+		observer.observe(img);
+	});
+});
